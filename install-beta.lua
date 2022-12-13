95244156be782ae3cca108be1ca398cd918ebaac
Tomo <68489118+Tomodachi94@users.noreply.github.com>
2022-12-13 06:58:21 -0800

    Add install-beta.lua

---
+++ install-beta.lua
@@ -0,0 +1,66 @@
+--- Installer for wing
+-- This program's strategy is to bootstrap the bare minimum required for `unicornpkg` to work,
+-- and then to install everything else.
+
+print("WARNING: This installer is not for public use.")
+print("There are no stability guarantees.")
+
+local shell = shell
+
+if _HOST:find("Recrafted") then
+	shell = require("shell")
+end
+
+local REPO = "https://raw.githubusercontent.com/unicornpkg/libunicornpkg/"
+local REF  = "v1.0.0/"
+local URL  = REPO .. REF
+
+local PACKAGES = "https://raw.githubusercontent.com/unicornpkg/wing/main/packages/"
+
+local folders = {
+	"/bin",
+	"/lib/unicorn/provider",
+	"/etc/unicorn/remotes",
+	"/etc/unicorn/remotes/installed",
+	"/usr/share/help"
+}
+
+local files = {
+	"lib/unicorn/init.lua",
+	"lib/unicorn/core.lua",
+	"lib/unicorn/util.lua",
+	"lib/unicorn/remote.lua",
+	"lib/unicorn/provider/com.github.lua"
+}
+
+for _, v in pairs(folders) do
+	shell.run("mkdir " .. v)
+end
+
+for _, v in pairs(files) do
+	shell.run("wget " .. URL .. v .. " " .. v)
+end
+
+local unicorn = dofile("/lib/unicorn/init.lua")
+
+local install_packages = {
+	loadstring(unicorn.util.smartHttp(PACKAGES .. "semver.lua")),
+	loadstring(unicorn.util.smartHttp(PACKAGES .. "unicornpkg.lua")),
+	loadstring(unicorn.util.smartHttp(PACKAGES .. "unicornpkg-provider-github.lua")),
+	loadstring(unicorn.util.smartHttp(PACKAGES .. "unicornpkg-provider-github-releases.lua")),
+	loadstring(unicorn.util.smartHttp(PACKAGES .. "unicornpkg-provider-github-gist.lua")),
+	loadstring(unicorn.util.smartHttp(PACKAGES .. "unicornpkg-provider-gitlab.lua")),
+	loadstring(unicorn.util.smartHttp(PACKAGES .. "unicornpkg-provider-pastebin.lua")),
+	loadstring(unicorn.util.smartHttp(PACKAGES .. "unicornpkg-provider-bitbucket.lua")),
+	loadstring(unicorn.util.smartHttp(PACKAGES .. "unicornpkg-provider-generic.lua")),
+	loadstring(unicorn.util.smartHttp(PACKAGES .. "unicornpkg-provider-string.lua")),
+	loadstring(unicorn.util.smartHttp(PACKAGES .. "unicorntool.lua")),
+	loadstring(unicorn.util.smartHttp(PACKAGES .. "hoof.lua")),
+}
+
+for _, v in install_packages do
+	unicorn.core.install(v)
+end
+
+-- setup default package remote
+unicorn.util.fileWrite("https://unicornpkg.github.io/unicornpkg-main", "/etc/unicorn/remotes/90_main.txt")
