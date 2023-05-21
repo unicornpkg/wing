fb2f162f2411a03aef62ac12537a6bfcf92e208f
Tomo <68489118+Tomodachi94@users.noreply.github.com>
2023-05-21 11:28:40 -0700

    chore: Automatically update installer

--- install.lua
+++ install.lua
@@ -1,3 +1,2 @@
-local TIMESTAMP = '1684642385'
-shell.run("wget run https://github.com/unicornpkg/libunicornpkg/releases/download/" .. TIMESTAMP .."/archive.sea")
-
+local TIMESTAMP = '1684693720'
+shell.run("wget run https://github.com/unicornpkg/wing/releases/download/" .. TIMESTAMP .. "/archive.sea")
