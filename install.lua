8b478e8f043c61dc8fa4e744348abb242effe18d
Tomo <68489118+tomodachi94@users.noreply.github.com>
2023-09-01 22:27:48 +0000

    Update install.lua

--- install.lua
+++ install.lua
@@ -1,2 +1,5 @@
 local TIMESTAMP = '1693605888'
-shell.run("wget run https://github.com/unicornpkg/wing/releases/download/" .. TIMESTAMP .. "/install.lua /")
+fs.makeDir("/tmp")
+shell.run("wget https://github.com/unicornpkg/wing/releases/download/" .. TIMESTAMP .. "/install.lua /tmp/install.lua")
+shell.run("/tmp/install.lua /")
+shell.run("rm /tmp/install.lua")
