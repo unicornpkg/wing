a05a1cad304b36a18bf923a04dce1f125b419498
Tomodachi94 <tomodachi94@protonmail.com>
2025-05-21 20:25:37 -0700

    Update install.lua

--- install.lua
+++ install.lua
@@ -1,4 +1,4 @@
-local TIMESTAMP = '1693605888'
+local TIMESTAMP = '1747884192'
 fs.makeDir("/tmp")
 shell.run("wget https://github.com/unicornpkg/wing/releases/download/" .. TIMESTAMP .. "/install.lua /tmp/install.lua")
 shell.run("/tmp/install.lua /")
