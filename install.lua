051aa194c966e8e7976b75c8c005ae53128fd04b
Tomodachi94 <tomodachi94@protonmail.com>
2025-06-30 19:45:10 -0700

    install.lua: fix download location

--- install.lua
+++ install.lua
@@ -1,5 +1,5 @@
 local TIMESTAMP = '1747884192'
 fs.makeDir("/tmp")
-shell.run("wget https://github.com/unicornpkg/wing/releases/download/" .. TIMESTAMP .. "/install.lua /tmp/install.lua")
+shell.run("wget https://github.com/unicornpkg/wing/releases/download/" .. TIMESTAMP .. "/install.lua.sfx /tmp/install.lua.sfx")
 shell.run("/tmp/install.lua /")
 shell.run("rm /tmp/install.lua")
