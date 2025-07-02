f41274b5cdb92b3f5abef10b3e0508dca260fdf0
Tomodachi94 <tomodachi94@protonmail.com>
2025-07-02 14:22:36 -0700

    install.lua: remove unnecessary timestamp setup
    We can use a `latest` endpoint, conviniently
    provided by GitHub.

--- install.lua
+++ install.lua
@@ -1,5 +1,4 @@
-local TIMESTAMP = '1747884192'
 fs.makeDir("/tmp")
-shell.run("wget https://github.com/unicornpkg/wing/releases/download/" .. TIMESTAMP .. "/install.lua.sfx /tmp/install.lua.sfx")
+shell.run("wget https://github.com/unicornpkg/wing/releases/latest/download/install.lua.sfx /tmp/install.lua.sfx")
 shell.run("/tmp/install.lua.sfx /")
 shell.run("rm /tmp/install.lua.sfx")
