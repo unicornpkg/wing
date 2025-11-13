582a3f7badd6249fae8bbe6080ea0d70668c3db8
Tomodachi94 <tomodachi94@protonmail.com>
2025-11-13 09:15:07 -0800

    Update install.lua

--- install.lua
+++ install.lua
@@ -5,5 +5,12 @@ shell.run("rm /tmp/install.lua.sfx")
 
 package.path = "/lib/?.lua;/lib/?;/lib/?/init.lua;" .. package.path
 local unicorn = require("unicorn")
+
+unicorn.core.install(
+	unicorn.util.evaluateInSandbox(
+		unicorn.util.smartHttp("https://unicornpkg.github.io/unicornpkg-main/unicorn-remote-main.lua")
+	)()
+)
+
 unicorn.remote.install("semver")
-unicorn.remote.install("sha256")
+unicorn.remote.install("anavrins-sha256")
