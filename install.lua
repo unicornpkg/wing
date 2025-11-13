1a0beaf0c4343c3d51ded5c191344d08c4b3f15b
Tomodachi94 <tomodachi94@protonmail.com>
2025-11-13 09:31:06 -0800

    Delete install.lua (moved to gh-pages branch)

--- install.lua
+++
@@ -1,4 +0,0 @@
-fs.makeDir("/tmp")
-shell.run("wget https://github.com/unicornpkg/wing/releases/latest/download/install.lua.sfx /tmp/install.lua.sfx")
-shell.run("/tmp/install.lua.sfx /")
-shell.run("rm /tmp/install.lua.sfx")
