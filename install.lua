d912aa109df04c173821b167c197cc1a42e6cfa5
Tomodachi94 <tomodachi94@protonmail.com>
2025-11-12 19:38:40 -0800

    Update install.lua

--- install.lua
+++ install.lua
@@ -2,3 +2,8 @@ fs.makeDir("/tmp")
 shell.run("wget https://github.com/unicornpkg/wing/releases/latest/download/install.lua.sfx /tmp/install.lua.sfx")
 shell.run("/tmp/install.lua.sfx /")
 shell.run("rm /tmp/install.lua.sfx")
+
+package.path = "/lib/?.lua;/lib/?;/lib/?/init.lua;" .. package.path
+local unicorn = require("unicorn")
+unicorn.remote.install("semver")
+unicorn.remote.install("sha256")
